41dadfd6c55ff0e1279d39059f7bca7623624268
Armando Roman <armandoroman1016@gmail.com>
2019-12-20 10:27:03 -0800

    mvp

--- names/names.py
+++ names/names.py
@@ -2,6 +2,8 @@ import time
 
 start_time = time.time()
 
+names = set()
+
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -11,10 +13,13 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    names.add(name_1)
+
+for name_2 in names_2:
+    if name_2 in names:
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
