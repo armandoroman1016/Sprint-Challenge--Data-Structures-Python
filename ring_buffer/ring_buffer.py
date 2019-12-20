1bd982232dcd8b171094ff9c2d5740520d6205fa
Armando Roman <armandoroman1016@gmail.com>
2019-12-20 10:03:07 -0800

    ring buffer complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,13 +6,40 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        self.oldest = None
 
     def append(self, item):
-        pass
+
+        if self.storage.length == 0:
+            self.storage.add_to_tail(item)
+            self.oldest = (self.storage.tail)
+
+        elif self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+
+        elif self.storage.length == self.capacity:
+
+            if self.oldest is not None:
+                x = self.oldest.next
+                self.oldest.value = item
+    
+            else:
+                x = self.storage.head.next
+                self.storage.head.value = item
+
+
+            self.oldest = x
+            
+            
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+        current = self.storage.head
+        while current is not None:
+
+            list_buffer_contents.append(current.value)
+            current = current.next
 
         # TODO: Your code here
 
