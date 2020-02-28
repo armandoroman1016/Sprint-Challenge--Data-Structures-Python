733b657e82dcceded5323234aefb5a91bfd1ccf5
Armando Roman <armandoroman1016@gmail.com>
2020-02-28 08:52:55 -0800

    mvp

--- names/names.py
+++ names/names.py
@@ -1,4 +1,6 @@
 import time
+# from .binary_seach_tree import BinarySearchTree
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,13 +12,22 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
+
+
+
+tree = BinarySearchTree("JUST A PLACEHOLDER")
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+for item_one in names_1:
+    tree.insert(item_one)
+
+for item_two in names_2:
+    if tree.contains(item_two):
+        duplicates.append(item_two)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
